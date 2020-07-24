92a7f22d6a76cf127f6ed6d89187c14df9103d0c
admin <daominhhiep97@gmail.com>
2020-07-24 11:04:54 +0700

    [Bài tập] Sử dụng lớp IllegalTriangleException

---
+++ src/Exercise.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercise  {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Hãy nhập x: ");
+            int x = scanner.nextInt();
+            System.out.println("Hãy nhập y: ");
+            int y = scanner.nextInt();
+            System.out.println("Hãy nhập z: ");
+            int z = scanner.nextInt();
+
+            Triangle triangle = new Triangle(x,y,z);
+        } catch (IllegalTriangleException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
